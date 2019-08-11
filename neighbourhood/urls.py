3e558b942100133e509bdc29b58af2cbece8dc50
muhindokiro <blaize1143@gmail.com>
2019-08-12 00:56:18 +0300

    in urls.py, create site path for index

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -4,15 +4,20 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-# urlpatterns=[
-#     url('^$',views.index,name = 'index'),
-#     url(r'^search/', views.search_results, name='search_results'),
-#     url(r'^profile$', views.profile, name='profile'),
-#     url(r'^home$', views.home, name='home'),
-#     url(r'^view$', views.view, name='view'),
-#     url(r'^new/project$', views.new_hood, name='new-hood'),
-#     url(r'^new/profile$', views.new_profile, name='new-profile')
-# ]
 
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
+
+
+urlpatterns=[
+    url('^$',views.index,name = 'index'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^profile$', views.profile, name='profile'),
+    url(r'^home$', views.home, name='home'),
+    url(r'^view$', views.view, name='view'),
+    url(r'^new/project$', views.new_hood, name='new-hood'),
+    url(r'^new/profile$', views.new_profile, name='new-profile')
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
