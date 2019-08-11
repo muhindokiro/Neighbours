01fabf7ecbaee7d338e52922287caa01611267d7
muhindokiro <blaize1143@gmail.com>
2019-08-12 00:37:20 +0300

    in class profile create class method def profile and save profile

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -25,7 +25,6 @@ class NeighbourHood(models.Model):
         neighbourhood = cls.objects.get(pk=id)
         return neighbourhood
 
-
 class Post(models.Model):
     post = models.CharField(max_length =60)
     pub_date = models.DateTimeField(auto_now_add=True)
