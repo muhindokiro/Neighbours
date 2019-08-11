38a95d13b0356e98e5d7c497a0c8d2843fd0ad4e
muhindokiro <blaize1143@gmail.com>
2019-08-12 00:27:53 +0300

    create models.py , create class neighbourhood,profile and post

---
+++ neighbourhood/models.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import User
+from django.db import models
+import datetime as dt
+
+# Create your models here.
+class NeighbourHood(models.Model):
+    neighbourhood_name = models.CharField(max_length =60)
+    location = models.CharField(max_length =60)
+    image = models.ImageField(upload_to='images/', blank=True)
+    occupants = models.IntegerField()
+
+    @classmethod
+    def home(cls):
+        neighbourhood = cls.objects.filter()
+        return neighbourhood
+
+    @classmethod
+    def search_by_neighbourhood_name(cls,search_term):
+        neighbourhood = cls.objects.filter(neighbourhood_name__icontains=search_term)
+        return neighbourhood
+    
+        
+    @classmethod
+    def get_neighbourhood_by_id(cls,id):
+        neighbourhood = cls.objects.get(pk=id)
+        return neighbourhood
+
+
+
+
+class Post(models.Model):
+    post = models.CharField(max_length =60)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+class Profile(models.Model):
+    profession = models.CharField(max_length =60)
+    status = models.CharField(max_length =60)
+    contact = models.IntegerField()
+    neighbourhood = models.CharField(max_length =60)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='profile/', blank=True)
+
+    @classmethod
+    def profile(cls):
+        profile = cls.objects.filter()
+        return profile
+
+    def save_profile(self):
+        self.save()
+
+
