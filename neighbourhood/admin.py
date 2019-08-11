92577c7b6d623d0ab2a6193ca6373c1af9bed7ed
muhindokiro <blaize1143@gmail.com>
2019-08-12 00:09:34 +0300

    register profile,post and neighbourhood class

--- neighbourhood/admin.py
+++ neighbourhood/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import NeighbourHood,Post,Profile
 
-# Register your models here.
+# Register models here.
 admin.site.register(NeighbourHood)
 admin.site.register(Post)
 admin.site.register(Profile)
