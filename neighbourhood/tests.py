22418d0e89e98a8545e6667c54ab3e6eee932e5c
muhindokiro <blaize1143@gmail.com>
2019-08-12 00:51:24 +0300

    create neighbourhoodtestclass def setup and teardown,, create posttestclass def setup and teardown,, create profiletestclass def setup and teardown

--- neighbourhood/tests.py
+++ neighbourhood/tests.py
@@ -1,4 +1,31 @@
 from django.test import TestCase
-from .models import NeighbourHood,Post
+from .models import NeighbourHood,Post,Profile
 
 # Create your tests here.
+class NeighbourHoodTestClass(TestCase):
+
+    def setUp(self):
+        self.new_neighbourhood= NeighbourHood(title = 'Test neighbourhood',neighbourhood = 'This is a random test NeighbourHood')
+        self.new_neighbourhood.save()
+
+    def tearDown(self):
+        NeighbourHood.objects.all().delete()
+
+class PostTestClass(TestCase):
+
+    def setUp(self):
+        self.new_post= Post(title = 'Test post',post = 'This is a random test post')
+        self.new_post.save()
+
+    def tearDown(self):
+        Post.objects.all().delete()
+
+class ProfileTestClass(TestCase):
+
+    def setUp(self):
+        self.new_profile= Profile(title = 'Test profile',profile = 'This is a random test Profile')
+        self.new_profile.save()
+
+
+    def tearDown(self):
+        Profile.objects.all().delete()
