0f324f0328eb4ebd348601225e1033eedbb5e603
muhindokiro <blaize1143@gmail.com>
2019-08-12 01:01:13 +0300

    in urls.py, add setting for media.url and media.root

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -3,15 +3,13 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-
 urlpatterns=[
     url('^$',views.index,name = 'index'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^profile$', views.profile, name='profile'),
     url(r'^home$', views.home, name='home'),
     url(r'^view$', views.view, name='view'),
-    url(r'^new/project$', views.new_hood, name='new-hood'),
+    url(r'^new/neighbourhood$', views.new_hood, name='new-hood'),
     url(r'^new/profile$', views.new_profile, name='new-profile')
 ]
 
