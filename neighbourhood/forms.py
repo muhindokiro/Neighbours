0151885dc5d1d48c90c323acbf528681049006db
muhindokiro <blaize1143@gmail.com>
2019-08-12 00:22:28 +0300

    in forms.py,create class newneighbourhoodform to have neighbourhood name, location, image and occupants

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -5,6 +5,7 @@ class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     email = forms.EmailField(label='Email')
 
+    
 class NewNeighbourHoodForm(forms.ModelForm):
     class Meta:
         model = NeighbourHood
