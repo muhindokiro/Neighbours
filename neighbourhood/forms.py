bfe9e7e617fb25ff385e3e4f2d7cd5d35cbfe64c
muhindokiro <blaize1143@gmail.com>
2019-08-12 00:21:09 +0300

    in forms.py,create class newsletterform to have name and email fields

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from .models import NeighbourHood,Post,Profile
 
-
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     email = forms.EmailField(label='Email')
