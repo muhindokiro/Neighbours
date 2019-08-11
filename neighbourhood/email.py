7ad4c58b5f3978e3338bb00ac9f9742053dbd6e3
muhindokiro <blaize1143@gmail.com>
2019-08-12 00:14:22 +0300

    create email.py, def send_welcome_email to have sender and subject as objects

---
+++ neighbourhood/email.py
@@ -0,0 +1,15 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+def send_welcome_email(name,receiver):
+    # Creating message subject and sender
+    subject = 'Welcome to Majirani'
+    sender = 'blaize1143@gmail.com'
+
+    #passing in the context vairables
+    text_content = render_to_string('email/neighbourmail.txt',{"name": name})
+    html_content = render_to_string('email/neighbourmail.html',{"name": name})
+
+    msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
+    msg.attach_alternative(html_content,'text/html')
+    msg.send()
