faa3628f777539a563301e9cf4bdf2b12605d629
muhindokiro <blaize1143@gmail.com>
2019-08-12 00:23:17 +0300

    in forms.py,create class newpostform, to have post field

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -5,13 +5,14 @@ class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     email = forms.EmailField(label='Email')
 
-    
+
 class NewNeighbourHoodForm(forms.ModelForm):
     class Meta:
         model = NeighbourHood
         fields = ('neighbourhood_name', 'image', 'location', 'occupants')
         exclude = ['pub_date']
 
+        
 class NewPostForm(forms.ModelForm):
     class Meta:
         model = Post
