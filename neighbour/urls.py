fa72af37c56d8cda32e6aa83d3240f09e49b6984
muhindokiro <blaize1143@gmail.com>
2019-08-11 22:57:55 +0300

    add url site path for logout page ,login page and tinymce

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth import views
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^',include('neighbourhood.urls')),
